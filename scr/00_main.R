324683d05f4a2ac11358d68dbd001abf44687122
Andreas Heinzel <1409997+aheinzel@users.noreply.github.com>
2023-08-18 11:08:15 +0200

    ensure required tmp and output folders are automtically created

--- scr/00_main.R
+++ scr/00_main.R
@@ -4,6 +4,8 @@
 # Info: 
 ###################################
 
+setwd("scr")
+
 rm(list=ls())
 
 library(HDMD)             #Contains Atchley factor data
@@ -45,16 +47,26 @@ path <- clean.path
 
 foldername_sim <- paste0("sim_run_n",nclones,"_",layout)
 out.path <- "../Output/"
+out.path.final = file.path(out.path, "_final") #required by report rmd
 if(dir.exists( paste0(out.path,foldername_sim,"/"))){
   do.call(file.remove, list(list.files(paste0(out.path,foldername_sim,"/"), full.names = TRUE)))
-}else{dir.create(paste(out.path,foldername_sim, sep=""))}
+}else{
+  dir.create(paste(out.path,foldername_sim, sep=""), recursive = T)
+}
 out.path <- paste0(out.path,foldername_sim,"/")
 
+if(!dir.exists(out.path.final)){
+  dir.create(out.path.final, recursive = T)
+}
+
+if(!dir.exists("../Daten/All_rerun_corr/clean_tcopies_fold5_freq1e-10_ar2")){ #ensure path used in data cleaning exists
+  dir.create("../Daten/All_rerun_corr/clean_tcopies_fold5_freq1e-10_ar2", recursive = T)
+}
 
 # ------- Execute code
 
 source("01_data_cleaning.R")
 
-source("02_network_analysis.R")
+#source("02_network_analysis.R")
 
-source("report_TCOPIES_pibl.Rmd")
+rmarkdown::render("report_TCOPIES_publ.Rmd", envir = new.env())
